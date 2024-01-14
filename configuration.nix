61a24250e3ce564bb3f1a77f5825d833bf236a69
acul009 <acul009@gmail.com>
2024-01-14 19:43:59 +0100

    enable flakes

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,8 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   boot.loader.grub.enable = true;
