9a9a1624e4640181a37de929b648c7b4541b65b8
acul009 <acul009@gmail.com>
2024-01-15 21:06:18 +0100

    rollback broken flake

--- flake.nix
+++ flake.nix
@@ -23,12 +23,6 @@
             ./configuration.nix
             # inputs.home-manager.nixosModules.default
           ];
-          config = {
-            # Additional packages without modifying configuration.nix
-            environment.systemPackages = [
-              pkgs.cowsay
-            ];
-          };
         };
 
     };
