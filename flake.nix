ca8f645728969bcc9a70c64d6e4ca679f6bb34f1
acul009 <acul009@gmail.com>
2024-01-15 18:39:59 +0100

    fix hostname

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     in
     {
     
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.acul-Battlestation= nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
             ./configuration.nix
