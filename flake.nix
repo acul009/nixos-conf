f6371b593236fabbe11c0ba7839959d1f826013e
wrong <acul009@gmail.com>
2024-01-18 20:39:08 +0100

    maybe like this?

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
