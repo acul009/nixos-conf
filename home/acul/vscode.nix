30d850119b3008a85bdfb2943f30bc3463b6dba6
wrong <acul009@gmail.com>
2024-01-19 23:44:37 +0100

    all extensions

--- home/acul/vscode.nix
+++ home/acul/vscode.nix
@@ -18,12 +18,56 @@
       # bungcip.better-toml
       # serayuzgur.crates
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+
+      # General
       {
         name = "vim";
         publisher = "vscodevim";
         version = "1.27.2";
         sha256 = "O5G4yhvD2HvKb4Vbvr1v20nMEQq88f5RE+X50bZvr1Q=";
       }
+      {
+        name = "git-graph";
+        publisher = "mhutchie";
+        version = "1.30.0";
+        sha256 = "sHeaMMr5hmQ0kAFZxxMiRk6f0mfjkg2XMnA4Gf+DHwA=";
+      }
+      {
+        name = "codeium";
+        publisher = "Codeium";
+        version = "1.6.24";
+        sha256 = "pPQaOXqkDf5F7BJkr4CPRdohlPxL3nyPPH+y+Uh0490=";
+      }
+
+      # Nix
+      {
+        name = "nix-ide";
+        publisher = "jnoortheen";
+        version = "0.2.2";
+        sha256 = "jwOM+6LnHyCkvhOTVSTUZvgx77jAg6hFCCpBqY8AxIg=";
+      }
+
+      # Rust
+      {
+        name = "rust-analyzer";
+        publisher = "rust-lang";
+        version = "0.3.1807";
+        sha256 = "4ZMBuDzUlaVXGerG2R2fwEzaHhIelUX4Bur8EaDbb4o=";
+      }
+      {
+        name = "better-toml";
+        publisher = "bungcip";
+        version = "0.3.2";
+        sha256 = "g+LfgjAnSuSj/nSmlPdB0t29kqTmegZB5B1cYzP8kCI=";
+      }
+      {
+        name = "crates";
+        publisher = "serayuzgur";
+        version = "0.6.5";
+        sha256 = "HgqM4PKGk3R5MLY4cVjKxv79p5KlOkVDeDbv7/6FmpM=";
+      }
+
+
     ];
 
     userSettings = {
