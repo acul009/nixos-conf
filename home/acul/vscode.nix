b027e1bf9de34685d13133aabc34588451578f37
wrong <acul009@gmail.com>
2024-01-19 22:40:36 +0100

    finally nice vscode config

--- home/acul/vscode.nix
+++ home/acul/vscode.nix
@@ -5,9 +5,22 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
+      # Basic
       mhutchie.git-graph
-
       vscodevim.vim
+      Codeium.codeium
+
+      # Nix & Nixos
+      jnoortheen.nix-ide
+
+      # Rust
+      rust-lang.rust-analyzer
+      bungcip.better-toml
+      serayuzgur.crates
     ];
+
+    userSettings = {
+      "editor.formatOnSave" = true;
+    };
   };
 }
