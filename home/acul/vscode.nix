526a04f42dc5c961b26c42fd40bcfb9b693cd01d
wrong <acul009@gmail.com>
2024-01-19 22:55:13 +0100

    comment extensions

--- home/acul/vscode.nix
+++ home/acul/vscode.nix
@@ -5,18 +5,18 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
-      # Basic
-      mhutchie.git-graph
-      vscodevim.vim
-      Codeium.codeium
+      # # Basic
+      # mhutchie.git-graph
+      # vscodevim.vim
+      # Codeium.codeium
 
-      # Nix & Nixos
-      jnoortheen.nix-ide
+      # # Nix & Nixos
+      # jnoortheen.nix-ide
 
-      # Rust
-      rust-lang.rust-analyzer
-      bungcip.better-toml
-      serayuzgur.crates
+      # # Rust
+      # rust-lang.rust-analyzer
+      # bungcip.better-toml
+      # serayuzgur.crates
     ];
 
     userSettings = {
