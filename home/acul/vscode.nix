8b4c68a654541f2fab3d9d868bb52fbdea100780
wrong <acul009@gmail.com>
2024-01-19 22:14:19 +0100

    fuck all of this

--- home/acul/vscode.nix
+++ home/acul/vscode.nix
@@ -1,35 +1,31 @@
 { inputs, config, pkgs, ... }:
 
 {
+  # imports = [
+  #   inputs.nix-vscode-extensions
+  # ];
 
 
-  imports = [
-    inputs.nix-vscode-extensions
-  ];
+  # programs.vscode = {
+  #   extensions = [
+  #     # Base
+  #     open-vsx.vscodevim.vim
+  #     # pkgs.vscode-extensions.mhutchie.git-graph
 
+  #     # "vscodevim.vim"
+  #     # "mhutchie.git-graph"
+  #     # "Codeium.codeium"
 
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscodium;
-    extensions = [
-      # Base
-      open-vsx.vscodevim.vim
-      # pkgs.vscode-extensions.mhutchie.git-graph
+  #     # # Nix & Nixos
+  #     # "jnoortheen.nix-ide"
 
-      # "vscodevim.vim"
-      # "mhutchie.git-graph"
-      # "Codeium.codeium"
-
-      # # Nix & Nixos
-      # "jnoortheen.nix-ide"
-
-      # # Rust
-      # "rust-lang.rust-analyzer"
-      # "bungcip.better-toml"
-      # "serayuzgur.crates"
-    ];
-    userSettings = {
-      "editor.formatOnSave" = true;
-    };
-  };
+  #     # # Rust
+  #     # "rust-lang.rust-analyzer"
+  #     # "bungcip.better-toml"
+  #     # "serayuzgur.crates"
+  #   ];
+  #   userSettings = {
+  #     "editor.formatOnSave" = true;
+  #   };
+  # };
 }
