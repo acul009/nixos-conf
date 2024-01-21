9a67cda0751fc1e168f8480a98c5d4de882b8bce
wrong <acul009@gmail.com>
2024-01-21 20:44:07 +0100

    fix flatpak name

--- home/acul/flatpaks.nix
+++ home/acul/flatpaks.nix
@@ -11,6 +11,6 @@
     "com.github.tchx84.Flatseal"
     "com.valvesoftware.Steam"
     "org.signal.Signal"
-    "flatpak run org.prismlauncher.PrismLauncher"
+    "org.prismlauncher.PrismLauncher"
   ];
 }
