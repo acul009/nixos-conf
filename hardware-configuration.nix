d9da58a35ad003125379294bc2ec02ee7be87620
wrong <acul009@gmail.com>
2024-02-09 00:24:01 +0100

    hardware log

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -53,4 +53,6 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  # log hardware errors
+  hardware.mcelog.enable = true;
 }
