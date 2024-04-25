b6550bdfedc1ca4ad1cf244bbb3741c6534e3107
wrong <acul009@gmail.com>
2024-04-26 00:03:01 +0200

    fix missing semikolon

--- home/acul/shell.nix
+++ home/acul/shell.nix
@@ -38,87 +38,87 @@
       os = {
         disabled = false;
         symbols = {
-          NixOs = "❄️ "
-            };
+          NixOs = "❄️ ";
         };
-
-        shlvl = {
-          disabled = false;
-          symbol = "λ ";
-        };
-
-        sudo = {
-          disabled = false;
-          format = "[$symbol]($style)";
-        };
-
-        username = {
-          disabled = false;
-          show_always = true;
-          format = "[$user]($style)";
-          style_user = "bright-white bold";
-          style_root = "bright-red bold";
-        };
-
-        shell = {
-          disabled = false;
-          format = "$indicator";
-          fish_indicator = "";
-          bash_indicator = "[BASH](bright-white) ";
-          zsh_indicator = "[ZSH](bright-white) ";
-        };
-
-        directory = {
-          disabled = false;
-          home_symbol = "🏠";
-        };
-
-        git_state = {
-          disabled = true;
-        };
-
-        git_status = {
-          disabled = true;
-        };
-
-        nix_shell = {
-          disabled = false;
-          impure_msg = "";
-          symbol = "❄️";
-          format = "[$symbol$state]($style) ";
-        };
-
-        haskell.symbol = " ";
       };
-    };
 
-    programs.dircolors.enable = true;
-    programs.dircolors.extraConfig = ''
-      TERM alacritty
-    '';
-    programs.dircolors.settings = {
-      ".iso" = "01;31"; # .iso files bold red like .zip and other archives
-      ".gpg" = "01;33"; # .gpg files bold yellow
-      # Images to non-bold magenta instead of bold magenta like videos
-      ".bmp" = "00;35";
-      ".gif" = "00;35";
-      ".jpeg" = "00;35";
-      ".jpg" = "00;35";
-      ".mjpeg" = "00;35";
-      ".mjpg" = "00;35";
-      ".mng" = "00;35";
-      ".pbm" = "00;35";
-      ".pcx" = "00;35";
-      ".pgm" = "00;35";
-      ".png" = "00;35";
-      ".ppm" = "00;35";
-      ".svg" = "00;35";
-      ".svgz" = "00;35";
-      ".tga" = "00;35";
-      ".tif" = "00;35";
-      ".tiff" = "00;35";
-      ".webp" = "00;35";
-      ".xbm" = "00;35";
-      ".xpm" = "00;35";
+      shlvl = {
+        disabled = false;
+        symbol = "λ ";
+      };
+
+      sudo = {
+        disabled = false;
+        format = "[$symbol]($style)";
+      };
+
+      username = {
+        disabled = false;
+        show_always = true;
+        format = "[$user]($style)";
+        style_user = "bright-white bold";
+        style_root = "bright-red bold";
+      };
+
+      shell = {
+        disabled = false;
+        format = "$indicator";
+        fish_indicator = "";
+        bash_indicator = "[BASH](bright-white) ";
+        zsh_indicator = "[ZSH](bright-white) ";
+      };
+
+      directory = {
+        disabled = false;
+        home_symbol = "🏠";
+      };
+
+      git_state = {
+        disabled = true;
+      };
+
+      git_status = {
+        disabled = true;
+      };
+
+      nix_shell = {
+        disabled = false;
+        impure_msg = "";
+        symbol = "❄️";
+        format = "[$symbol$state]($style) ";
+      };
+
+      haskell.symbol = " ";
     };
-  }
+  };
+
+  programs.dircolors.enable = true;
+  programs.dircolors.extraConfig = ''
+    TERM alacritty
+  '';
+  programs.dircolors.settings = {
+    ".iso" = "01;31"; # .iso files bold red like .zip and other archives
+    ".gpg" = "01;33"; # .gpg files bold yellow
+    # Images to non-bold magenta instead of bold magenta like videos
+    ".bmp" = "00;35";
+    ".gif" = "00;35";
+    ".jpeg" = "00;35";
+    ".jpg" = "00;35";
+    ".mjpeg" = "00;35";
+    ".mjpg" = "00;35";
+    ".mng" = "00;35";
+    ".pbm" = "00;35";
+    ".pcx" = "00;35";
+    ".pgm" = "00;35";
+    ".png" = "00;35";
+    ".ppm" = "00;35";
+    ".svg" = "00;35";
+    ".svgz" = "00;35";
+    ".tga" = "00;35";
+    ".tif" = "00;35";
+    ".tiff" = "00;35";
+    ".webp" = "00;35";
+    ".xbm" = "00;35";
+    ".xpm" = "00;35";
+  };
+}
