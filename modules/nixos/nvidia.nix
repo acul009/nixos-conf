8c5cdf3d20c31ec4aca4f20219d4c7e323b094f9
wrong <acul009@gmail.com>
2024-05-03 22:55:48 +0200

    Revert "fucking nvidia"
    This reverts commit ba8c2e7b8d80775143d15ab20ef5171d947e9afa.

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -19,46 +19,43 @@ with lib;
       # Load nvidia driver for Xorg and Wayland
       services.xserver.videoDrivers = [ "nvidia" ];
 
-      hardware.nvidia =
-        {
+      hardware.nvidia = {
 
-          # Modesetting is required.
-          modesetting.enable = true;
+        # Modesetting is required.
+        modesetting.enable = true;
 
-          # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-          powerManagement.enable = false;
-          # Fine-grained power management. Turns off GPU when not in use.
-          # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-          powerManagement.finegrained = false;
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        powerManagement.enable = false;
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
 
-          # Use the NVidia open source kernel module (not to be confused with the
-          # independent third-party "nouveau" open source driver).
-          # Support is limited to the Turing and later architectures. Full list of 
-          # supported GPUs is at: 
-          # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-          # Only available from driver 515.43.04+
-          # Currently alpha-quality/buggy, so false is currently the recommended setting.
-          open = false;
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of 
+        # supported GPUs is at: 
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        # Only available from driver 515.43.04+
+        # Currently alpha-quality/buggy, so false is currently the recommended setting.
+        open = false;
 
-          # Enable the Nvidia settings menu,
-          # accessible via `nvidia-settings`.
-          nvidiaSettings = true;
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
 
-          # causes flickering
-          package = config.boot.kernelPackages.nvidiaPackages.stable;
-        }
+        # causes flickering
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
 
-          # hardware acceleration
-          environment.systemPackages = with pkgs;
-      [
-      libva-utils
-        vdpauinfo
+        # hardware acceleration
+        environment.systemPackages = with pkgs; [
+          libva-utils
+          vdpauinfo
         ];
-        })
+      })
 
-        (mkIf config.services.displayManager.sddm.enable {
-        # sddm with wayland doesn't work with nvidia
-        services.displayManager.sddm.wayland.enable = mkForce false;
+      (mkIf config.services.displayManager.sddm.enable {
+      # sddm with wayland doesn't work with nvidia
+      services.displayManager.sddm.wayland.enable = mkForce false;
       services.xserver.enable = true;
     })
 
