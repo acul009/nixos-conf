12cc6adfc5292bbd3e8cd39bce300c5e3cc27dee
wrong <acul009@gmail.com>
2024-05-06 20:06:39 +0200

    fix hyprland build

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/Hyprland";
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
