2cbce26ea74735e6204c091ac5a393c83ffdb5fa
wrong <acul009@gmail.com>
2024-05-26 13:45:37 +0200

    full refresh rate

--- users/acul.nix
+++ users/acul.nix
@@ -13,7 +13,7 @@
       monitors = [
         "desc:Iiyama North America PLX2472HC 1153270400628, 1920x1080@60, 0x460, 1"
 
-        "desc:Microstep MSI MAG321CQR KA3H028500018, 2560x1440@120, 1920x0, 1"
+        "desc:Microstep MSI MAG321CQR KA3H028500018, 2560x1440@144, 1920x0, 1"
       ];
     };
     rustup.enable = true;
