ba1b11093eeca7f5266322b98083cf8856c69200
wrong <acul009@gmail.com>
2024-05-26 23:37:07 +0200

    fix zsh keybinds

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -14,123 +14,128 @@ with lib;
     programs.zsh = {
       enable = true;
       autosuggestion.enable = true;
+
+      initExtra = ''
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+      '';
     };
 
     programs.starship.enableZshIntegration = true;
     programs.dircolors.enableZshIntegration = true;
 
     programs.starship = {
-    enable = true;
-    settings = {
+      enable = true;
+      settings = {
 
-      add_newline = false;
-      format = ''
-        $fill   \[ $status $cmd_duration\]   $fill
-         $os $shlvl$username@$shell$directory$git_branch$git_commit$git_state$git_status$jobs$sudo$nix_shell$character
-      '';
+        add_newline = false;
+        format = ''
+          $fill   \[ $status $cmd_duration\]   $fill
+           $os $shlvl$username@$shell$directory$git_branch$git_commit$git_state$git_status$jobs$sudo$nix_shell$character
+        '';
 
-      fill = {
-        symbol = "-";
-        style = "";
-      };
-
-      status = {
-        disabled = false;
-        success_symbol = "✅";
-        format = "[$symbol]($style)";
-      };
-
-      cmd_duration = {
-        disabled = false;
-        min_time = 0;
-      };
-
-      os = {
-        disabled = false;
-        symbols = {
-          NixOS = "❄️ ";
+        fill = {
+          symbol = "-";
+          style = "";
         };
-      };
 
-      shlvl = {
-        disabled = false;
-        symbol = "λ ";
-      };
+        status = {
+          disabled = false;
+          success_symbol = "✅";
+          format = "[$symbol]($style)";
+        };
 
-      sudo = {
-        disabled = false;
-        format = "[$symbol]($style)";
-      };
+        cmd_duration = {
+          disabled = false;
+          min_time = 0;
+        };
 
-      username = {
-        disabled = false;
-        show_always = true;
-        format = "[$user]($style)";
-        style_user = "bright-white bold";
-        style_root = "bright-red bold";
-      };
+        os = {
+          disabled = false;
+          symbols = {
+            NixOS = "❄️ ";
+          };
+        };
 
-      shell = {
-        disabled = false;
-        format = "$indicator";
-        fish_indicator = "";
-        bash_indicator = "[BASH](bright-white) ";
-        zsh_indicator = "[ZSH](bright-white) ";
-      };
+        shlvl = {
+          disabled = false;
+          symbol = "λ ";
+        };
 
-      directory = {
-        disabled = false;
-        home_symbol = "🏠";
-      };
+        sudo = {
+          disabled = false;
+          format = "[$symbol]($style)";
+        };
 
-      git_state = {
-        disabled = true;
-      };
+        username = {
+          disabled = false;
+          show_always = true;
+          format = "[$user]($style)";
+          style_user = "bright-white bold";
+          style_root = "bright-red bold";
+        };
 
-      git_status = {
-        disabled = true;
-      };
+        shell = {
+          disabled = false;
+          format = "$indicator";
+          fish_indicator = "";
+          bash_indicator = "[BASH](bright-white) ";
+          zsh_indicator = "[ZSH](bright-white) ";
+        };
 
-      nix_shell = {
-        disabled = false;
-        impure_msg = "";
-        symbol = "❄️ ";
-        format = "[$symbol$state]($style) ";
-      };
+        directory = {
+          disabled = false;
+          home_symbol = "🏠";
+        };
 
-      haskell.symbol = " ";
+        git_state = {
+          disabled = true;
+        };
+
+        git_status = {
+          disabled = true;
+        };
+
+        nix_shell = {
+          disabled = false;
+          impure_msg = "";
+          symbol = "❄️ ";
+          format = "[$symbol$state]($style) ";
+        };
+
+        haskell.symbol = " ";
+      };
     };
-  };
 
-  programs.dircolors.enable = true;
-  programs.dircolors.extraConfig = ''
-    TERM alacritty
-  '';
-  programs.dircolors.settings = {
-    ".iso" = "01;31"; # .iso files bold red like .zip and other archives
-    ".gpg" = "01;33"; # .gpg files bold yellow
-    # Images to non-bold magenta instead of bold magenta like videos
-    ".bmp" = "00;35";
-    ".gif" = "00;35";
-    ".jpeg" = "00;35";
-    ".jpg" = "00;35";
-    ".mjpeg" = "00;35";
-    ".mjpg" = "00;35";
-    ".mng" = "00;35";
-    ".pbm" = "00;35";
-    ".pcx" = "00;35";
-    ".pgm" = "00;35";
-    ".png" = "00;35";
-    ".ppm" = "00;35";
-    ".svg" = "00;35";
-    ".svgz" = "00;35";
-    ".tga" = "00;35";
-    ".tif" = "00;35";
-    ".tiff" = "00;35";
-    ".webp" = "00;35";
-    ".xbm" = "00;35";
-    ".xpm" = "00;35";
-  };
+    programs.dircolors.enable = true;
+    programs.dircolors.extraConfig = ''
+      TERM alacritty
+    '';
+    programs.dircolors.settings = {
+      ".iso" = "01;31"; # .iso files bold red like .zip and other archives
+      ".gpg" = "01;33"; # .gpg files bold yellow
+      # Images to non-bold magenta instead of bold magenta like videos
+      ".bmp" = "00;35";
+      ".gif" = "00;35";
+      ".jpeg" = "00;35";
+      ".jpg" = "00;35";
+      ".mjpeg" = "00;35";
+      ".mjpg" = "00;35";
+      ".mng" = "00;35";
+      ".pbm" = "00;35";
+      ".pcx" = "00;35";
+      ".pgm" = "00;35";
+      ".png" = "00;35";
+      ".ppm" = "00;35";
+      ".svg" = "00;35";
+      ".svgz" = "00;35";
+      ".tga" = "00;35";
+      ".tif" = "00;35";
+      ".tiff" = "00;35";
+      ".webp" = "00;35";
+      ".xbm" = "00;35";
+      ".xpm" = "00;35";
+    };
   };
 
 }
