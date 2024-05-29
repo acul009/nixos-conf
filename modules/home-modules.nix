93eda3049a9c5dc55ddf68b2d7ed0a0ad0ffb361
wrong <acul009@gmail.com>
2024-05-30 00:28:45 +0200

    auto import

--- modules/home-modules.nix
+++ modules/home-modules.nix
@@ -1,15 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
-  imports = [
-    ./home/discord.nix
-    ./home/firefox.nix
-    ./home/heroic.nix
-    ./home/hyprland.nix
-    ./home/kde-tools.nix
-    ./home/network-manager.nix
-    ./home/rustup.nix
-    ./home/spotify.nix
-    ./home/vscode.nix
-    ./home/zsh.nix
-  ];
+  imports = builtins.filter (lib.hasSuffix ".nix") (lib.filesystem.listFilesRecursive ./home);
 }
--- modules/nixos-modules.nix
+++ modules/nixos-modules.nix
@@ -1,23 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
-  imports = [
-    ./nixos/allow-unfree.nix
-    ./nixos/boot.nix
-    ./nixos/cli-tools.nix
-    ./nixos/default-timezone.nix
-    ./nixos/docker.nix
-    ./nixos/experimental-features.nix
-    ./nixos/git.nix
-    ./nixos/hyprland.nix
-    ./nixos/locale.nix
-    ./nixos/networking.nix
-    ./nixos/nvidia.nix
-    ./nixos/plasma.nix
-    ./nixos/printing.nix
-    ./nixos/scanning.nix
-    ./nixos/sound.nix
-    ./nixos/steam.nix
-    ./nixos/user.nix
-    ./nixos/zsh.nix
-  ];
+  imports = builtins.filter (lib.hasSuffix ".nix") (lib.filesystem.listFilesRecursive ./nixos);
 }
