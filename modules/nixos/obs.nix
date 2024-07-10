b401ddb2c27528589c8b1db65a217ed1605ae7da
wrong <acul009@gmail.com>
2024-07-10 22:39:46 +0200

    fixed obs

--- modules/nixos/obs.nix
+++ modules/nixos/obs.nix
@@ -1,27 +1,28 @@
 { config, lib, pkgs, inputs, ... }:
 
+with lib;
 {
   options.woelfchen.obs = {
     enable = mkEnableOption "";
   };
 
   config = mkIf config.woelfchen.obs.enable {
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
-  security.polkit.enable = true;
+    boot.extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    boot.extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
+    security.polkit.enable = true;
 
-  environment.systemPackages = [
-    (pkgs.wrapOBS {
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-backgroundremoval
-        obs-pipewire-audio-capture
-      ];
-    })
-  ];
-};
+    environment.systemPackages = [
+      (pkgs.wrapOBS {
+        plugins = with pkgs.obs-studio-plugins; [
+          wlrobs
+          obs-backgroundremoval
+          obs-pipewire-audio-capture
+        ];
+      })
+    ];
+  };
 }
